1c046b9116f025bef075b415ed9725b65a43edb3
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2019-10-03 19:11:56 +0530

    Create CountNODELeaf.java

---
+++ Tree/CountNODELeaf.java
@@ -0,0 +1,27 @@
+public class Solution {
+
+/*	TreeNode structure 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	public static int count = 0;
+	public static int countLeafNodes(TreeNode<Integer> root){
+
+		// Write your code here
+if (root.children.size() == 0) {
+			count++;
+		}
+		for (int i = 0; i < root.children.size(); i++) {
+			countLeafNodes(root.children.get(i));
+		}
+return count;
+	}
+	
+}

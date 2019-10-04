a90006da7ec0ae520fb42dfd833b1ae7415b89f3
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2019-10-04 12:59:27 +0530

    Create NodeinaGivenRanga.java

---
+++ BST/NodeinaGivenRanga.java
@@ -0,0 +1,32 @@
+public class Solution {
+
+/*	Binary Tree Node class 
+ * 
+ * 	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+		
+		public BinaryTreeNode(T data) {
+			this.data = data;
+		}
+	}
+	*/
+	
+	public static void printNodeFromK1ToK2(BinaryTreeNode<Integer> root,int k1,int k2){
+        if(root == null){
+            return ;
+        }
+	if(root.data<k1){
+        printNodeFromK1ToK2(root.right,k1,k2);
+        
+    } else if(root.data>k2){
+        printNodeFromK1ToK2(root.left,k1,k2);
+    }
+        else {
+            printNodeFromK1ToK2(root.left,k1,k2);
+            System.out.print(root.data+" ");
+            printNodeFromK1ToK2(root.right,k1,k2);
+        }
+    }	
+}

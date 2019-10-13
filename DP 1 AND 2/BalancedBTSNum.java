477aae33aaafa003b607c9679070ddeb46aa8822
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2019-10-13 15:00:46 +0530

    Create BalancedBTSNum.java

---
+++ DP 1 AND 2/BalancedBTSNum.java
@@ -0,0 +1,37 @@
+public class Solution {
+
+	public static int balancedTreesOfHeightH(int height){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+  int modValue = (int)Math.pow(10, 9) + 7;
+		
+		
+if(height==0 || height==1) {
+			
+return 1;
+		}
+		
+int x = balancedTreesOfHeightH(height-1);
+		
+int y = balancedTreesOfHeightH(height-2);
+		
+		
+long ans1 = ((long)x*x)%(modValue);
+		
+long ans2 = (2*x*(long)y)%(modValue);
+		
+		
+int res1 = (int) ans1;
+		
+int res2 = (int) ans2;
+		
+		
+int ans = (res1 + res2)%(modValue);
+		
+return ans;
+	}
+}

e7caacc31d9da734528ab7db657f0abc94d7d67a
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2020-08-18 14:19:33 +0530

    Update ArraySum.java

--- Array1/ArraySum.java
+++ Array1/ArraySum.java
@@ -5,12 +5,7 @@ public class Solution {
 
 	
     public static int sum(int[] input){
-		/* Your class should be named Solution
-	 	* Don't write main().
-	 	* Don't read input, it is passed as function argument.
-	 	* Return output and don't print it.
-		* Taking input and printing output is handled automatically.
-		*/	
+		
 		int retVal = Sum(input);
         return retVal;
 			

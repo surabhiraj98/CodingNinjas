da1a78c293ebc667774c2377fbbb4b7dfaeffe1d
PranavShekhar30 <66740189+PranavShekhar30@users.noreply.github.com>
2020-10-03 01:23:33 +0530

    Update sort012.java
    Optimal Solution of this code. Method known as Dutch Nationam Flag Algorithm. Improves Time Complexity as O(n) and space complexity as O(1).

--- Array2/sort012.java
+++ Array2/sort012.java
@@ -22,39 +22,66 @@ public class Sort012{
 		 * No need to print or return the output.
 		 * Taking input and printing output is handled automatically.
 		 */
-		int n1 = 0,n2 = 0,n3 = 0,i,j;
-	for(i = 0;i < arr.length;i++) {
-		if(arr[i] == 0)
-			n1++;
-		if(arr[i] == 1)
-			n2++;
-		if(arr[i] == 2)
-			n3++;	
-	}   
-	    i = 0;
-	    j = 0;
-		while(j < n1) {
-			arr[i] = 0;
-			i++;
-			j++;
-		}
-		j = 0;
-		while(j < n2) {
-			arr[i] = 1;
-			i++;
-			j++;
-		}
-		j = 0;
-		while(j < n3) {
-		    arr[i] = 2;
-		    i++;
-		    j++;
-		}
-	}
+// 		int n1 = 0,n2 = 0,n3 = 0,i,j;
+// 	for(i = 0;i < arr.length;i++) {
+// 		if(arr[i] == 0)
+// 			n1++;
+// 		if(arr[i] == 1)
+// 			n2++;
+// 		if(arr[i] == 2)
+// 			n3++;	
+// 	}   
+// 	    i = 0;
+// 	    j = 0;
+// 		while(j < n1) {
+// 			arr[i] = 0;
+// 			i++;
+// 			j++;
+// 		}
+// 		j = 0;
+// 		while(j < n2) {
+// 			arr[i] = 1;
+// 			i++;
+// 			j++;
+// 		}
+// 		j = 0;
+// 		while(j < n3) {
+// 		    arr[i] = 2;
+// 		    i++;
+// 		    j++;
+// 		}
+// 	}
 
-	public static void main(String[] args) {
-       int[] arr = takeInput();
-       sort012(arr);
-printArray(arr);
-	}
+// 	public static void main(String[] args) {
+//        int[] arr = takeInput();
+//        sort012(arr);
+// printArray(arr);
+// 	}
+// }
+
+    public void sortColors(int[] arr) {
+       int low=0,mid=0,high=arr.length-1;
+        while(mid<=high){
+            
+            if(arr[mid]==0){
+                int temp=arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+                mid++;
+                low++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            
+            else{
+                int temp=arr[high];
+                arr[high]=arr[mid];
+                arr[mid]=temp;
+                high--;
+            }
+        }
+        
+    }
 }
+

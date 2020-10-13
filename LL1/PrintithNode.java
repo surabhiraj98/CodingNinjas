2e72b2dfae61dbc0040c8ca874ecabf7367eb12b
Bhavya Kalra <71188028+Bkalra08@users.noreply.github.com>
2020-10-13 20:08:28 +0530

    simplifying the code
    simplifying the code and easy to understand with description of every step.

--- LL1/PrintithNode.java
+++ LL1/PrintithNode.java
@@ -19,20 +19,28 @@ public class Solution {
 		 * Print output and don't return it.
 	 	 * Taking input is handled automatically.
 		*/
-		LinkedListNode<Integer> temp = head;
-		int count = 0;
-		while (temp != null) {
-			if (count == i) {
-				System.out.println(temp.data);
-				return;
-			} else {
-				count++;
-				temp = temp.next;
-				if (count == i) {
-					System.out.println(temp.data);
-					return;
-				}
-			}
-		}
+// 		LinkedListNode<Integer> temp = head;
+// 		int count = 0;
+// 		while (temp != null) {
+// 			if (count == i) {
+// 				System.out.println(temp.data);
+// 				return;
+// 			} else {
+// 				count++;
+// 				temp = temp.next;
+// 				if (count == i) {
+// 					System.out.println(temp.data);
+// 					return;
+// 				}
+// 			}
+// 		}
+			int position = 0;  /*initial position is 0 and will be getting updated after each iteration */
+		while(temp != null && position < i){
+			temp = temp.next;   /*the present temp is pointing towards the next one */
+			position++;         /*position is moved/increased by one*/
+		}/*we can use position +=1 also*/
+		if(temp = null ){
+			System.out.println(temp.data);
+		}      /*the while loop will keep executing till we reach the end element i.e the ith element*/
 	}
 }
